b70ba53fff893bb78df63124183e6adfd7e885be
Charles Rockenbach <charlesrockenbach@gmail.com>
2012-07-10 22:07:28 -0300

    Página de listar produtos

--- modules/produtos/views/produtos/index.php
+++ modules/produtos/views/produtos/index.php
@@ -33,47 +33,49 @@
 </div>
 
 <div style="clear: both"></div>
+<div class="data">
+    <table class="table table-striped">
+        <thead>
+        <th>Nome</th>
+        <th>Preço</th>
+        <th>Quantidade</th>
+        <th class="span2">Ações</th>
+        </thead>
+        <tbody>
+            <? foreach ($model as $produto): ?>
+                <tr>
+                    <td><?= $produto->nome ?></td>
+                    <td><?= $produto->preco ?></td>
+                    <td><?= $produto->quantidade ?></td>
+                    <td>
+                        <a href="#" class="btn tip" title="Editar"><i class="icon-pencil"></i></a>
+                        <a href="#" class="btn tip" title="Visualizar"><i class="icon-eye-open"></i></a>
+                        <a href="#" class="btn tip" title="Excluir"><i class="icon-trash"></i></a>
+                    </td>
+                </tr>
+            <? endforeach; ?>
+        </tbody>
+        <tfoot>
+        <th>Nome</th>
+        <th>Preço</th>
+        <th>Quantidade</th>
+        <th class="span2">Ações</th>
+        </tfoot>
+    </table>
 
-<table class="table table-striped">
-    <thead>
-    <th>Nome</th>
-    <th>Preço</th>
-    <th>Quantidade</th>
-    <th class="span2">Ações</th>
-</thead>
-<tbody>
-    <? foreach ($model as $produto): ?>
-        <tr>
-            <td><?= $produto->nome ?></td>
-            <td><?= $produto->preco ?></td>
-            <td><?= $produto->quantidade ?></td>
-            <td>
-                <a href="#" class="btn tip" title="Editar"><i class="icon-pencil"></i></a>
-                <a href="#" class="btn tip" title="Visualizar"><i class="icon-eye-open"></i></a>
-                <a href="#" class="btn tip" title="Excluir"><i class="icon-trash"></i></a>
-            </td>
-        </tr>
-    <? endforeach; ?>
-</tbody>
-<tfoot>
-<th>Nome</th>
-<th>Preço</th>
-<th>Quantidade</th>
-<th class="span2">Ações</th>
-</tfoot>
-</table>
+    <a href="~/produtos?q=<?= $query ?>&pg=0&r=json" class="btn btn-mini pagination-btn tip" id="0" title="Primeira"><i class="icon-fast-backward"></i></a>
 
+    <? for ($a = $pg - 3 < 1 ? 1 : $pg - 3; $a <= $pg + 3 && $a <= $qt_pg; $a++): ?>
+        <a href="~/produtos?q=<?= $query ?>&pg=<?= $a ?>&r=json" class="btn btn-mini pagination-btn <?= $a == $pg + 1 ? 'active' : '' ?>" id="<?= $pg ?>">
+            <?= $a ?>
+        </a>
+    <? endfor; ?>
 
-<? for ($a; $a < $qt_pg; $a++): ?>
-    <
-<? endfor; ?>
-
-
+    <a href="~/produtos?q=<?= $query ?>&pg=<?= $qt_pg - 1 ?>&r=json" class="btn btn-mini pagination-btn tip" title="Última"><i class="icon-forward"></i></a>
+</div>
 
 <script type="text/javascript">
     $(document).ready(function(){
-        
-        
         
         $('#search-btn').toggle(function(){
             $('#search-div').slideDown('fast');
@@ -82,6 +84,16 @@
             $('#search-div').slideUp('fast');
         });
         
+        $('.pagination-btn').click(function(){
+            $.ajax({
+                url: $(this).attr('href'),
+                success: function(data){
+                    var tbody = $('.table > tbody');
+                    tbody.empty();
+                    
+                }
+            });
+        });
     });
 </script>
 
