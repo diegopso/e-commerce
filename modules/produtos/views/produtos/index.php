ece27f534a76a59f6d1a3bfe0048f29c4bb2638a
Charles Rockenbach <charlesrockenbach@gmail.com>
2012-07-11 01:58:33 -0300

    View index do ProdutoController finalizada

--- modules/produtos/views/produtos/index.php
+++ modules/produtos/views/produtos/index.php
@@ -16,8 +16,8 @@
 
 <div class="clearfix"></div>
 
-<div id="search-div" style="display: none" class="well">
-    <h3>Busca</h3><br />
+<form id="search-div" style="display: none" class="well">
+    <h3>Buscar</h3><br />
     <div class="btn-group pull-left">
         <button class="btn dropdown-toggle span3" data-toggle="dropdown">
             <span class="pull-left">Categorias</span>
@@ -32,12 +32,22 @@
         <button class="btn btn-primary" type="button" id="search-submit"><i class="icon-search icon-white"></i> Buscar</button>
     </div>
     <div class="clearfix"></div>
+</form>
+
+<div class="alert alert-block" style="display: none">
+    <center>
+        <strong>
+            <a class="close" data-dismiss="alert" href="#">×</a>
+            O produto foi excluído. 
+            <a class="btn btn-warning" href="javascript:void(0);">Desfazer</a>
+        </strong>
+    </center>
 </div>
 
 <div style="clear: both"></div>
 <div class="data">
     <?php
-        include('listaprodutos.php');
+    include('listaprodutos.php');
     ?>
 </div>
 
@@ -49,7 +59,7 @@
             $(this).find('i').removeClass('icon-chevron-down').addClass('icon-chevron-up');
         },
         function(){
-            $('#search-div').slideUp('fast');
+            $('#search-div').slideUp('fast').clear();
             $(this).find('i').removeClass('icon-chevron-up').addClass('icon-chevron-down');
         });
         
@@ -62,8 +72,28 @@
         
         $('.delete').click(function(e){
             e.preventDefault();
-            
+            $.ajax({
+                url: $(this).attr('href'),
+                success: function(data){
+                    var divAlert = $('.alert');
+                    divAlert.find('a').click(function(e){ 
+                        e.preventDefault();
+                        $.ajax({
+                            url: 'produtos/desfazerexclusao/' + data.d.model.id,
+                            success: function(){
+                                divAlert.slideUp('fast');
+                            }
+                        });
+                    });
+                    divAlert.slideDown('fast');
+                    setTimeout(function(){
+                        divAlert.slideUp('fast');
+                        requestData('produtos?q=' + $('#search-text').val() +'&r=html');
+                    }, 2000);
+                }
+            });
         });
+        
     });
     
     function loadData(e){
